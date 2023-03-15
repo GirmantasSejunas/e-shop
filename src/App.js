43d7cdf1c2ab4ddecc88c31973faa45270dcc3bf
Girmantas <girmantas123@gmail.com>
2023-03-15 09:16:23 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path='/'/>
+        <Route path='/cart'/>
+      </Routes>
+      
+      </BrowserRouter>
+
       e-shop page
     </div>
   );
