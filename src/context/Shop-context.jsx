e94e2792b0a60db91d9538d00f39b4284ba23e98
Girmantas <girmantas123@gmail.com>
2023-03-15 12:14:44 +0200

    created shop context

--- src/context/Shop-context.jsx
+++ src/context/Shop-context.jsx
@@ -1,8 +1,34 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
+import {PRODUCTS} from '../products'
+
 export const ShopContext = createContext(null);
 
-export const ShopContextProvider = () => {
+const getDefaultCart = () => {
+
+  let cart = {}
+  for(let i = 1; i < PRODUCTS.length + 1; i++){
+    cart[i] = 0
+  }
+  return cart
+}
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart())
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
+  }
+
+  const contexValue = {cartItems, addToCart, removeFromCart}
+
+
   return (
-    <div>shop-context</div>
+    <div>
+     <ShopContext.Provider value={contexValue}>{props.children}</ShopContext.Provider> 
+      </div>
   )
 }
